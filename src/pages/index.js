c8a933be85578afb42b1bf05f8f9da2de2d3872a
max-jellycat <max@jellycat.fr>
2019-02-22 01:31:25 +0100

    test webhooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,8 @@ import React from 'react'
 
 import '../styles/index.scss';
 
-export default () => <div className="title">Hello world!</div>
+export default () => (
+  <div>
+    <h1 className="title">Hello world!</h1>
+  </div>
+)
